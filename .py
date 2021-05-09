9eca76863dcf9b7d8a4cb38e24518b48a9b5954b
Oisin Brenno <15obrennan@stjosephsrush.ie>
2021-05-09 15:21:05 +0100

    Update .py

--- .py
+++ .py
@@ -13,55 +13,46 @@ peoplecount = 0                           #sets the peoplecount to 0 on startup
 
 welcome = input("Welcome to the mask break calculator, do you wish to continue and start the program? Please type 'Y' for yes or anything else to kill the program")   #welcome message 
 
-if welcome=="Y":
+if welcome=="Y":                          #starts the program if requested
   
   sleep(0.5)
   while True:                               #everything after this loops 
       humid, temp = DHT.read_retry(DHT.DHT11, 4)                         #sets up humidity and temperature sensor
-      #print(humid, temp)
       result = firebase.patch('/sensor/dht/', {'Temperature': temp, 'Humidity': humid})       #sends the results of the sensor to firebase
       sleep(1)
 
-      sensor = GPIO.input(23)
-      #print(sensor)
+      sensor = GPIO.input(23)              #gets light sensor data
 
-
-      if sensor ==1:
-          #print("That is someone")
+      if sensor ==1:                       #if positive signal sent from sensor
           sleep(1)
-          peoplecount += 1
-          result = firebase.patch('/sensor/', {'Number of People': peoplecount})
+          peoplecount += 1                 #adds one to the peoplecount
+          result = firebase.patch('/sensor/', {'Number of People': peoplecount})              #sends the new peoplecount to the firebase
     
-      elif sensor==0:
-          #print("No one there.")
+      elif sensor==0:                     #if negative singal sent from sensor
           sleep(1)
-          result = firebase.patch('/sensor/', {'Number of People': peoplecount})
+          result = firebase.patch('/sensor/', {'Number of People': peoplecount})              #semds the current peoplecount to firebase
         
       print("The latest temperature was:")
-      resulttemp = firebase.get('/sensor/dht/Temperature', None)
-      print(resulttemp)
+      resulttemp = firebase.get('/sensor/dht/Temperature', None)                         #gets data from firebase, temperature
+      print(resulttemp)                                                                  #prints this data
     
       sleep(.2)
     
       print("The latest humidity was:")
-      resulthumid = firebase.get('/sensor/dht/Humidity', None)
-      print(resulthumid)
-    
+      resulthumid = firebase.get('/sensor/dht/Humidity', None)                           #gets data from firebase, humidity
+      print(resulthumid)                                                                 #prints this data
+     
       sleep(.2)
     
       print("The latest people count was:")
-      resultpeep = firebase.get('/sensor/Number of People', None)
-      print(resultpeep)
+      resultpeep = firebase.get('/sensor/Number of People', None)                        #gets data from firebase, light sensor
+      print(resultpeep)                                                                  #prints this data
       sleep(.2)
     
-      #resulttemp = 20
-      #resulthumid = 5
-      #resultpeep = 20
-      #equation
       print("Now for the equation...")
-      timeish = (((resultpeep*2)/(resulttemp*resulthumid))*3)
-      print((round(timeish*10))/10)
-      print("... hours until next mask break")
+      timeish = (((resultpeep*2)/(resulttemp*resulthumid))*3)                            #equation for the final output made from several data points
+      print((round(timeish*10))/10)                                                      #second part to equation and prints it
+      print("... hours until next mask break")                                           #repeats the process
       
-else:
+else:                               #otherwise if answer is not yes, program terminates
   exit()
